f6f5af18c41c7a28303abd79871524b3d99eaa8f
Peter Borissow <peter.borissow@yahoo.com>
2020-04-28 05:29:33 -0400

    Updated models

--- models/models.js
+++ models/models.js
@@ -3,24 +3,29 @@ var models = {
 
 
   //**************************************************************************
-  //** Source
+  //** Transaction
   //**************************************************************************
-  /** Used to represent a bank or credit card (e.g. Wells Fargo, AMEX, etc).
+  /** Used to represent a bank or credit card transaction. Transactions come
+   *  from a variety of Sources and are linked to individual Accounts.
    */
-    Source: {
+    Transaction: {
         fields: [
-            {name: 'name',          type: 'string'},
+            {name: 'date',          type: 'date'},
             {name: 'description',   type: 'string'},
-            {name: 'active',        type: 'boolean'},
-            {name: 'accountNumber', type: 'string'},
+            {name: 'notes',         type: 'string'},
+            {name: 'amount',        type: 'decimal'},
+            {name: 'rawData',       type: 'string'},
+            {name: 'source',        type: 'Source'},
+            {name: 'vendor',        type: 'Vendor'},
+            {name: 'category',      type: 'Category'},
             {name: 'info',          type: 'json'}
         ],
         constraints: [
-            {name: 'name',          required: true,  length: 75,  unique: true},
-            {name: 'active',        required: true}
-        ],
-        defaults: [
-            {name: 'active',    value: true}
+            {name: 'date',          required: true},
+            {name: 'amount',        required: true},
+            {name: 'rawData',       unique: true},
+            {name: 'source',        required: true},
+            {name: 'category',      onDelete: 'set null'}
         ]
     },
 
@@ -28,7 +33,7 @@ var models = {
   //**************************************************************************
   //** Account
   //**************************************************************************
-  /** Used to represent an account (e.g. Personal, ACME Consulting, etc)
+  /** Used to represent an account into which (e.g. Personal Account, Business Account, etc) Used to group transactions
    */
     Account: {
         fields: [
@@ -50,7 +55,7 @@ var models = {
   //**************************************************************************
   //** Category
   //**************************************************************************
-  /** Travel, Food/Groceries, Advertising, etc
+  /** Used to tag individual transactions (e.g. Travel, Food/Groceries, Advertising, etc)
    */
     Category: {
         fields: [
@@ -64,6 +69,63 @@ var models = {
             {name: 'name',      required: true,  length: 75},
             {name: 'isExpense', required: true},
             {name: 'account',   required: true, onDelete: 'cascade'}
+        ]
+    },
+
+
+  //**************************************************************************
+  //** Source
+  //**************************************************************************
+  /** Used to represent a source of a transaction (bank account, credit card,
+   *  etc)
+   */
+    Source: {
+        fields: [
+            {name: 'account',       type: 'SourceAccount'},
+            {name: 'template',      type: 'SourceTemplate'}
+        ]
+    },
+
+
+  //**************************************************************************
+  //** SourceAccount
+  //**************************************************************************
+    SourceAccount: {
+        fields: [
+            {name: 'vendor',         type: 'Vendor'}, //Wells Fargo, AMEX, etc
+            {name: 'accountName',    type: 'string'}, //Business Checking, Platinum Card, etc
+            {name: 'accountNumber',  type: 'string'}, //1111-1111-1111-1111
+            {name: 'active',         type: 'boolean'},
+            {name: 'info',           type: 'json'}
+        ],
+        constraints: [
+            {name: 'accountName',    required: true,  length: 75},
+            {name: 'accountNumber',  required: true,  length: 125},
+            {name: 'active',         required: true}
+        ],
+        defaults: [
+            {name: 'active',  value: true}
+        ]
+    },
+
+
+  //**************************************************************************
+  //** SourceTemplate
+  //**************************************************************************
+    SourceTemplate: {
+        fields: [
+            {name: 'vendor',        type: 'Vendor'}, //Wells Fargo, AMEX, etc
+            {name: 'name',          type: 'string'},
+            {name: 'description',   type: 'string'},
+            {name: 'active',        type: 'boolean'},
+            {name: 'info',          type: 'json'}
+        ],
+        constraints: [
+            {name: 'name',    required: true,  length: 75,  unique: true},
+            {name: 'active',  required: true}
+        ],
+        defaults: [
+            {name: 'active',  value: true}
         ]
     },
 
@@ -84,33 +146,6 @@ var models = {
         ],
         defaults: [
             {name: 'active',        value: true}
-        ]
-    },
-
-
-  //**************************************************************************
-  //** Transaction
-  //**************************************************************************
-  /** Used to represent a bank or credit card transaction
-   */
-    Transaction: {
-        fields: [
-            {name: 'date',          type: 'date'},
-            {name: 'description',   type: 'string'},
-            {name: 'notes',         type: 'string'},
-            {name: 'amount',        type: 'decimal'},
-            {name: 'rawData',       type: 'string'},
-            {name: 'source',        type: 'Source'},
-            {name: 'vendor',        type: 'Vendor'},
-            {name: 'category',      type: 'Category'},
-            {name: 'info',          type: 'json'}
-        ],
-        constraints: [
-            {name: 'date',          required: true},
-            {name: 'amount',        required: true},
-            {name: 'rawData',       unique: true},
-            {name: 'source',        required: true},
-            {name: 'category',      onDelete: 'set null'}
         ]
     },
 
