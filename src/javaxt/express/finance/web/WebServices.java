791846c27ae102faab9109ee8c77d9b0a9890d73
Peter Borissow <peter.borissow@yahoo.com>
2020-05-29 17:25:07 -0400

    Updated the addTransactions() method in the WebServices class to use the new Parser class

--- src/javaxt/express/finance/web/WebServices.java
+++ src/javaxt/express/finance/web/WebServices.java
@@ -1,6 +1,5 @@
 package javaxt.express.finance.web;
 import javaxt.express.finance.*;
-import javaxt.express.utils.CSV;
 
 import javaxt.express.WebService;
 import javaxt.express.ServiceResponse;
@@ -14,11 +13,6 @@ import javaxt.http.servlet.*;
 import java.util.*;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.math.BigDecimal;
-
-import javax.script.*;
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-
 
 //******************************************************************************
 //**  WebServices
@@ -211,114 +205,19 @@ public class WebServices extends WebService {
         try{
             JSONObject json = request.getJson();
             long sourceID = json.get("source").toLong();
+            String[] data = json.get("data").toString().split("\n");
+
+
             Source source = new Source(sourceID);
-            JSONObject template = source.getTemplate().getInfo();
-            int startRow = template.has("startRow") ? template.get("startRow").toInteger() : 0;
-            Boolean containsHeader = template.get("containsHeader").toBoolean();
-            if (containsHeader==null) containsHeader = false;
-            String columnParser = template.get("columnParser").toString();
-            String dateFormat = template.get("dateFormat").toString();
-            int dateColumn = template.get("dateColumn").toInteger();
-            int descColumn = template.get("descColumn").toInteger();
-            int debitColumn = template.get("debitColumn").toInteger();
-            int creditColumn = template.get("creditColumn").toInteger();
+            javaxt.express.finance.utils.Parser parser =
+            new javaxt.express.finance.utils.Parser(source.getTemplate());
 
-
-            int offset = startRow;
-            if (offset<1) offset = 0;
-            else offset = offset-1;
-            if (containsHeader) offset++;
-
-
-            ScriptObjectMirror parser = null;
-            if (columnParser!=null){
-                ScriptEngineManager factory = new ScriptEngineManager();
-                ScriptEngine engine = factory.getEngineByName("nashorn");
-                Compilable compilable = (Compilable) engine;
-                CompiledScript script = compilable.compile(columnParser);
-                Bindings bindings = engine.createBindings();
-                script.eval(bindings);
-                parser = (ScriptObjectMirror) bindings.get("parseColumns");
-            }
 
             JSONArray errors = new JSONArray();
-
-            String[] data = json.get("data").toString().split("\n");
-            for (int i=offset; i<data.length; i++){
-                String row = data[i];
-                if (row.endsWith("\r")) row = row.substring(0, row.length()-1);
-                if (row.isEmpty()) continue;
-
-
-                CSV.Columns columns = CSV.getColumns(row, ",");
-
-
-
-                if (parser!=null){
-
-                  //Convert the columns to a string array
-                    String[] cols = new String[columns.length()];
-                    for (int j=0; j<cols.length; j++){
-                        cols[j] = columns.get(j).toString();
-                    }
-
-
-                  //Execute script
-                    ScriptObjectMirror output = (ScriptObjectMirror) parser.call(null, Arrays.asList(cols));
-                    Object[] arr = output.to(Object[].class);
-
-
-                  //Update columns
-                    columns = new CSV.Columns();
-                    for (Object obj : arr){
-                        columns.add(new Value(obj));
-                    }
-                }
-
-
-
-
-                String dateVal = columns.get(dateColumn).toString().trim();
-                while (dateVal.contains("  ")) dateVal = dateVal.replace("  ", " ");
-                javaxt.utils.Date date;
-                if (dateFormat==null) date = new javaxt.utils.Date(dateVal);
-                else date = new javaxt.utils.Date(dateVal, dateFormat);
-
-                String desc = columns.get(descColumn).toString();
-                if (desc.startsWith("\"") && desc.endsWith("\"")){
-                    desc = desc.substring(1, desc.length()-1);
-                }
-
-                BigDecimal amount;
-                if (debitColumn==creditColumn){
-                    amount = columns.get(debitColumn).toBigDecimal();
-                }
-                else{
-                    String debit = columns.get(debitColumn).toString();
-                    String credit = columns.get(creditColumn).toString();
-
-                    if (debit==null) debit = ""; else debit = debit.trim();
-                    if (credit==null) credit = ""; else credit = credit.trim();
-
-                    if (debit.equalsIgnoreCase("undefined")) debit = "";
-                    if (credit.equalsIgnoreCase("undefined")) credit = "";
-
-                    if (debit.length()>0){
-                        amount = new BigDecimal(debit);
-                        if (amount.compareTo(BigDecimal.ZERO) > 0) amount = amount.negate();
-                    }
-                    else{
-                        amount = new BigDecimal(credit);
-                    }
-                }
-
+            int x = 0;
+            for (Transaction tx : parser.getTransactions(data)){
                 try{
-                    Transaction tx = new Transaction();
                     tx.setSource(source);
-                    tx.setDate(date);
-                    tx.setDescription(desc);
-                    tx.setAmount(amount);
-                    tx.setRawData(row);
                     tx.save();
                 }
                 catch(SQLException e){
@@ -326,9 +225,10 @@ public class WebServices extends WebService {
                     if (msg.length()>100) msg = msg.substring(0, 100);
                     JSONObject err = new JSONObject();
                     err.set("msg", msg);
-                    err.set("idx", i);
+                    err.set("idx", x);
                     errors.add(err);
                 }
+                x++;
             }
 
             if (errors.isEmpty()){
